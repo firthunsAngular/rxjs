599a702477cce5d047f382f59041f7a08482e375
Firthuns <firthuns@gmail.com>
2023-12-04 21:14:27 +0100

    proyecto base inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {SidebarComponent} from "./layout/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
